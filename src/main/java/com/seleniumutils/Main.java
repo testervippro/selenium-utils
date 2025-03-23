9d445c7616ec6b3776e3085d466a624b57d2504a
testervippro <92687973+testervippro@users.noreply.github.com>
2025-03-23 22:41:18 +0700

    Update Main.java

--- src/main/java/com/seleniumutils/Main.java
+++ src/main/java/com/seleniumutils/Main.java
@@ -1,7 +1,16 @@
 package com.seleniumutils;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void hideDock() throws IOException {
+        String[] command = {"osascript", "-e", "tell application \"System Events\" to set autohide of dock preferences to true"};
+        Runtime.getRuntime().exec(command);
+        System.out.println("Dock hidden.");
     }
+
+    public static void showDock() throws IOException {
+        String[] command = {"osascript", "-e", "tell application \"System Events\" to set autohide of dock preferences to false"};
+        Runtime.getRuntime().exec(command);
+        System.out.println("Dock shown.");
+    }
+
 }
